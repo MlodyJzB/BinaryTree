7dbfae0d486ad0ecdfe6c6dfd2fa4f20b7253160
MlodyJzB <cimochowski01@gmail.com>
2022-11-24 21:26:52 +0200

    Add initN() function

--- BinaryTree/binaryTree.c
+++ BinaryTree/binaryTree.c
@@ -8,12 +8,26 @@ struct Node {
 
 void printActions();
 
-void ascendAdd(struct Node* newN, struct Node** root);
+void initN(float num, struct Node* n);
+
+void ascendAdd(struct Node* toAdd, struct Node** root);
 
 struct Node* ascendPosPar(struct Node* toAdd, struct Node* start);
 
 int main() {
+	struct Node* root = NULL;
+	struct Node n1;
+	n1.num = 10;
+	
+	struct Node n2;
+	n2.num = 9;
 
+	struct Node n3;
+	n3.num = 11;
+
+	ascendAdd(&n1, &root);
+	ascendAdd(&n2, &root);
+	ascendAdd(&n3, &root);
 }
 
 void printActions() {
@@ -21,8 +35,14 @@ void printActions() {
 	printf("Possible actions: \n1 - Add item\n2 - Delete item\n3 - Find item\n4 - Edit item\n5 - Show all items from beginning\n6 - Show all items from end\n0 - Exit\n");
 }
 
+void initN(float num, struct Node* n) {
+	n->num = num;
+	n->left = NULL;
+	n->right = NULL;
+}
+
 void ascendAdd(struct Node* toAdd, struct Node** root) {
-	if (*root) {
+	if (!(*root)) {
 		*root = toAdd;
 	}
 	else {
